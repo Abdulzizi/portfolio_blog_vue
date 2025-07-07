2da562cf5e4048c489ac7facef1da0cddf3085b3
Abdul Jawad Azizi <jawadabdul307@gmail.com>
2025-07-07 16:43:38 +0700

    Add vercel Analytic

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { Toaster } from "vue-sonner";
-
+import { Analytics } from "@vercel/analytics/next"
 </script>
 
 <template>
+  <Analytics />
   <RouterView />
   <Toaster richColors />
 </template>
